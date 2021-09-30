53f21711b8a5087388cb0d63e4a0dc9925c8d7c7
Courtney Downs <downscourtney17@gmail.com>
2021-09-30 16:47:57 -0400

    implemented AddToOpen() for AStarSearch

--- AStarSearch/main.cpp
+++ AStarSearch/main.cpp
@@ -3,48 +3,38 @@
 #include <sstream>
 #include <string>
 #include <vector>
-using std::abs;
 using std::cout;
 using std::ifstream;
 using std::istringstream;
 using std::string;
 using std::vector;
+using std::abs;
 
-enum class State
-{
-  kEmpty,
-  kObstacle
-};
+enum class State {kEmpty, kObstacle, kClosed};
 
-vector<State> ParseLine(string line)
-{
-  istringstream sline(line);
-  int n;
-  char c;
-  vector<State> row;
-  while (sline >> n >> c && c == ',')
-  {
-    if (n == 0)
-    {
-      row.push_back(State::kEmpty);
+
+vector<State> ParseLine(string line) {
+    istringstream sline(line);
+    int n;
+    char c;
+    vector<State> row;
+    while (sline >> n >> c && c == ',') {
+      if (n == 0) {
+        row.push_back(State::kEmpty);
+      } else {
+        row.push_back(State::kObstacle);
+      }
     }
-    else
-    {
-      row.push_back(State::kObstacle);
-    }
-  }
-  return row;
+    return row;
 }
 
-vector<vector<State>> ReadBoardFile(string path)
-{
-  ifstream myfile(path);
+
+vector<vector<State>> ReadBoardFile(string path) {
+  ifstream myfile (path);
   vector<vector<State>> board{};
-  if (myfile)
-  {
+  if (myfile) {
     string line;
-    while (getline(myfile, line))
-    {
+    while (getline(myfile, line)) {
       vector<State> row = ParseLine(line);
       board.push_back(row);
     }
@@ -52,50 +42,64 @@ vector<vector<State>> ReadBoardFile(string path)
   return board;
 }
 
-// TODO: Write the Search function stub here.
-vector<vector<State>> Search(vector<vector<State>> board,
-                             vector<int> startCoordinates, vector<int> goalCoordinates)
-{
 
-  vector<vector<State>> result = {};
-
-  cout << "No path found!";
-  return result;
+// Calculate the manhattan distance
+int Heuristic(int x1, int y1, int x2, int y2) {
+  return abs(x2 - x1) + abs(y2 - y1);
 }
 
-string CellString(State cell)
-{
+// TODO: Write the AddToOpen function here.
+void AddToOpen(int x, int y, int g, int h, vector<vector<int>> &openNodes,
+    vector<vector<State>> &grid){
 
-  switch (cell)
-  {
-  case State::kObstacle:
-    return "⛰️   ";
-  default:
-    return "0   ";
+      // 1. Create a vector<int> node with the form {x,y,g,h} and push 
+      //      the node to the back of the open vector.
+      vector<int> openNode = {x,y,g,h};
+      openNodes.push_back(openNode);
+
+      // 2. Set the grid value for the x and y coordinates to the enum value kClosed. 
+      grid[x][y] = State::kClosed;
+
+}
+
+
+/** 
+ * Implementation of A* search algorithm
+ */
+vector<vector<State>> Search(vector<vector<State>> grid, int init[2], int goal[2]) {
+
+
+  cout << "No path found!" << "\n";
+  return std::vector<vector<State>> {};
+}
+
+
+string CellString(State cell) {
+  switch(cell) {
+    case State::kObstacle: return "⛰️   ";
+    default: return "0   "; 
   }
 }
 
-void PrintBoard(const vector<vector<State>> board)
-{
-  for (int i = 0; i < board.size(); i++)
-  {
-    for (int j = 0; j < board[i].size(); j++)
-    {
+
+void PrintBoard(const vector<vector<State>> board) {
+  for (int i = 0; i < board.size(); i++) {
+    for (int j = 0; j < board[i].size(); j++) {
       cout << CellString(board[i][j]);
     }
     cout << "\n";
   }
 }
 
-int main()
-{
-  // TODO: Declare "init" and "goal" arrays with values {0, 0} and {4, 5} respectively.
-  vector<int> init = {};
-  vector<int> goal = {};
+#include "test.cpp"
 
+int main() {
+  int init[2]{0, 0};
+  int goal[2]{4, 5};
   auto board = ReadBoardFile("1.board");
-  // TODO: Call Search with "board", "init", and "goal". Store the results in the variable "solution".
   auto solution = Search(board, init, goal);
-  // TODO: Change the following line to pass "solution" to PrintBoard.
   PrintBoard(solution);
+  // Tests
+  TestHeuristic();
+  TestAddToOpen();
 }
