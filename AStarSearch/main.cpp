0e81b781f2dbf57d898b2e28d76b215a4cbb34e5
Courtney Downs <downscourtney17@gmail.com>
2021-09-30 11:56:29 -0400

    created search function stub

--- AStarSearch/main.cpp
+++ AStarSearch/main.cpp
@@ -3,40 +3,48 @@
 #include <sstream>
 #include <string>
 #include <vector>
+using std::abs;
 using std::cout;
 using std::ifstream;
 using std::istringstream;
 using std::string;
 using std::vector;
-using std::abs;
 
+enum class State
+{
+  kEmpty,
+  kObstacle
+};
 
-
-enum class State {kEmpty, kObstacle};
-
-
-vector<State> ParseLine(string line) {
-    istringstream sline(line);
-    int n;
-    char c;
-    vector<State> row;
-    while (sline >> n >> c && c == ',') {
-      if (n == 0) {
-        row.push_back(State::kEmpty);
-      } else {
-        row.push_back(State::kObstacle);
-      }
+vector<State> ParseLine(string line)
+{
+  istringstream sline(line);
+  int n;
+  char c;
+  vector<State> row;
+  while (sline >> n >> c && c == ',')
+  {
+    if (n == 0)
+    {
+      row.push_back(State::kEmpty);
     }
-    return row;
+    else
+    {
+      row.push_back(State::kObstacle);
+    }
+  }
+  return row;
 }
 
-
-vector<vector<State>> ReadBoardFile(string path) {
-  ifstream myfile (path);
+vector<vector<State>> ReadBoardFile(string path)
+{
+  ifstream myfile(path);
   vector<vector<State>> board{};
-  if (myfile) {
+  if (myfile)
+  {
     string line;
-    while (getline(myfile, line)) {
+    while (getline(myfile, line))
+    {
       vector<State> row = ParseLine(line);
       board.push_back(row);
     }
@@ -45,30 +53,49 @@ vector<vector<State>> ReadBoardFile(string path) {
 }
 
 // TODO: Write the Search function stub here.
+vector<vector<State>> Search(vector<vector<State>> board,
+                             vector<int> startCoordinates, vector<int> goalCoordinates)
+{
 
+  vector<vector<State>> result = {};
 
-string CellString(State cell) {
-  switch(cell) {
-    case State::kObstacle: return "⛰️   ";
-    default: return "0   "; 
+  cout << "No path found!";
+  return result;
+}
+
+string CellString(State cell)
+{
+
+  switch (cell)
+  {
+  case State::kObstacle:
+    return "⛰️   ";
+  default:
+    return "0   ";
   }
 }
 
-
-void PrintBoard(const vector<vector<State>> board) {
-  for (int i = 0; i < board.size(); i++) {
-    for (int j = 0; j < board[i].size(); j++) {
+void PrintBoard(const vector<vector<State>> board)
+{
+  for (int i = 0; i < board.size(); i++)
+  {
+    for (int j = 0; j < board[i].size(); j++)
+    {
       cout << CellString(board[i][j]);
     }
     cout << "\n";
   }
 }
 
-
-int main() {
+int main()
+{
   // TODO: Declare "init" and "goal" arrays with values {0, 0} and {4, 5} respectively.
+  vector<int> init = {};
+  vector<int> goal = {};
+
   auto board = ReadBoardFile("1.board");
   // TODO: Call Search with "board", "init", and "goal". Store the results in the variable "solution".
+  auto solution = Search(board, init, goal);
   // TODO: Change the following line to pass "solution" to PrintBoard.
-  PrintBoard(board);
+  PrintBoard(solution);
 }
