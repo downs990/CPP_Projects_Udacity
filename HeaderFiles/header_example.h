f70f4d23877670ac383e14acbc89bef3f058b1a1
Courtney Downs <downscourtney17@gmail.com>
2021-10-01 13:14:13 -0400

    Added header files example project

---
+++ HeaderFiles/header_example.h
@@ -0,0 +1,26 @@
+
+#ifndef HEADER_EXAMPLE_H
+#define HEADER_EXAMPLE_H
+/* ^^^ 
+    This is called an "include guard". Since the header will be 
+    included into another file, and #include just pastes contents 
+    into a file, the include guard prevents the same file from being 
+    pasted multiple times into another file. 
+    
+    This might happen if multiple files include the same header, and then are all 
+    included into the same main.cpp, for example. 
+    
+    The ifndef checks if HEADER_EXAMPLE_H has not been defined in the file already.
+    If it has not been defined yet, then it is defined with 
+    #define HEADER_EXAMPLE_H, and the rest of the header is used.
+    If HEADER_EXAMPLE_H has already been defined, then the
+    preprocessor does not enter the ifndef block. 
+     
+*/
+
+int IncrementAndComputeVectorSum(vector<int> v);
+void AddOneToEach(vector<int> &v);
+
+#endif
+
+ 
