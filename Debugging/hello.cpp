285445c86f60d306ce9dfdc0935f29cbea0e5f92
Courtney Downs <downscourtney17@gmail.com>
2021-10-09 21:34:03 -0400

    added console input example code

--- Debugging/hello.cpp
+++ Debugging/hello.cpp
@@ -1,15 +1,28 @@
 #include <iostream>
 #include <string>
 #include <vector>
+using std::cout;
+using std::cin;
 
 int main(){
 
     std::vector<std::string> brothers{"David", "Ethan", "Adam"};
+
+
+    
+
     for (std::string const& brother : brothers)
     {
-        std::cout << "Hello " << brother << "!\n";
+        cout << "Hello " << brother << "!\n";
+       
     }
 
+
+    int number = 0;
+    cout << "Please enter a number: ";
+    
+    cin >> number;
+    cout << "Number entered: " << number; 
 
 
 
