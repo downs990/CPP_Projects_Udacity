0292d602e520cfd55ec684d7816da4404fc05e2e
Courtney Downs <downscourtney17@gmail.com>
2021-10-22 16:06:51 -0400

    Added friend class example code

---
+++ Friends/main.cpp
@@ -0,0 +1,26 @@
+#include <cassert>
+
+class Heart{
+    private: 
+        int rate{80};
+
+        // Allows Human to have direct access to the private variable 'rate'. 
+        friend class Human;
+};
+
+class Human{
+    public:
+        Heart heart;
+        void Exercise(){heart.rate = 150;}
+        int HeartRate(){return heart.rate;}
+};
+
+
+
+
+int main(){
+    Human human;
+    assert(human.HeartRate() == 80);
+    human.Exercise();
+    assert(human.HeartRate() == 150);
+}
