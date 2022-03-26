3abc4c3216c6eeec1e4f8284e52182d2f6666d50
Courtney Downs <downscourtney17@gmail.com>
2022-03-26 10:24:03 -0400

    updating example code for copying policies

---
+++ CopyingPolicies/deep_copy.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+class DeepCopy
+{
+private:
+    int *_myInt;
+
+public:
+    DeepCopy(int val)
+    {
+        _myInt = (int *)malloc(sizeof(int));
+        *_myInt = val;
+        std::cout << "resource allocated at address " << _myInt << std::endl;
+    }
+    ~DeepCopy()
+    {
+        free(_myInt);
+        std::cout << "resource freed at address " << _myInt << std::endl;
+    }
+	
+	// Both the assignment operator and the copy constructor have been overloaded 
+	// with the source object passed by reference. 
+	
+	// Steps: 
+	// 1. A proprietary block of memory is allocated on the heap. 
+	// 2. The content of the source is copied into it. 
+    DeepCopy(DeepCopy &source)
+    {
+        _myInt = (int *)malloc(sizeof(int));
+        *_myInt = *source._myInt;
+        std::cout << "resource allocated at address " << _myInt << " with _myInt = " << *_myInt << std::endl;
+    }
+    DeepCopy &operator=(DeepCopy &source)
+    {
+        _myInt = (int *)malloc(sizeof(int));
+        std::cout << "resource allocated at address " << _myInt << " with _myInt=" << *_myInt << std::endl;
+        *_myInt = *source._myInt;
+        return *this;
+    }
+};
+
+int main()
+{
+    DeepCopy source(42);
+    DeepCopy dest1(source);
+    DeepCopy dest2 = dest1;
+
+    return 0;
+}
--- CopyingPolicies/exclusive_ownership.cpp
+++ CopyingPolicies/exclusive_ownership.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 
+
+
+// What is ExclusiveCopy ? 
+// Whenever a resource management object is copied, the resource handle is transferred from the
+// source pointer to the destination pointer. The source pointer is set to nullptr to make
+// ownership exclusive. At any time, the resource handle belongs only to a single object, which
+// is responsible for its deletion when it is no longer needed.
 class ExclusiveCopy
 {
 private:
@@ -13,6 +20,8 @@ public:
     }
     ~ExclusiveCopy()
     {
+		// This prevents a double deletion error. It will only be deleted if 
+		// it has Not alreaady been deleted. 
         if (_myInt != nullptr)
         {
             free(_myInt);
@@ -23,7 +32,14 @@ public:
     ExclusiveCopy(ExclusiveCopy &source)
     {
         _myInt = source._myInt;
-        source._myInt = nullptr;
+        source._myInt = nullptr; // Prevents 2 variables (handles )from having access to the same memory in the heap.
+		
+		
+		
+		// One problem in this implementation is that for a short time there are effectively
+		// two valid handles to the same resource - after the handle has been copied and before 
+		// it is set to nullptr. In concurrent programs, this would cause a data race for the resource. 
+		//A much better alternative to handle exclusive ownership in C++ would be to use "move semantics".
     }
     ExclusiveCopy &operator=(ExclusiveCopy &source)
     {
