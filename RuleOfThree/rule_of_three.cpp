63a320f9c70b10f3fe353604afd2d5fe3e98aacb
Courtney Downs <downscourtney17@gmail.com>
2022-04-06 18:10:14 -0400

    learning more about the rule of five

--- RuleOfThree/rule_of_three.cpp
+++ RuleOfThree/rule_of_three.cpp
@@ -8,6 +8,14 @@ private:
     int *_data;
 
 public:
+
+	// 1 : destructor
+    ~MyMovableClass()                                           
+    {
+        std::cout << "DELETING instance of MyMovableClass at " << this << std::endl;
+        delete[] _data;
+    }
+	
     MyMovableClass(size_t size) 
     {
         _size = size;
@@ -16,7 +24,8 @@ public:
                   << " allocated with size = " << _size*sizeof(int)  << " bytes" << std::endl;
     }
     
-    MyMovableClass(const MyMovableClass &source)            // 2 : copy constructor
+	// 2 : copy constructor
+    MyMovableClass(const MyMovableClass &source)             
     {
         _size = source._size;
         _data = new int[_size];
@@ -24,8 +33,8 @@ public:
         std::cout << "COPYING content of instance " << &source << " to instance " << this << std::endl;
     }
 
-    
-    MyMovableClass &operator=(const MyMovableClass &source)   // 3 : copy assignment operator
+    // 3 : copy assignment operator
+    MyMovableClass &operator=(const MyMovableClass &source)    
     {
         std::cout << "ASSIGNING content of instance " << &source << " to instance " << this << std::endl;
         
@@ -40,11 +49,36 @@ public:
     }
 	
 	
-    ~MyMovableClass()                                          // 1 : destructor
+	// 4 : move constructor
+	MyMovableClass(MyMovableClass &&source)  
     {
-        std::cout << "DELETING instance of MyMovableClass at " << this << std::endl;
-        delete[] _data;
+        std::cout << "MOVING (c’tor) instance " << &source << " to instance " << this << std::endl;
+        _data = source._data;
+        _size = source._size;
+        source._data = nullptr;
+        source._size = 0;
     }
+	
+	// 5 : move assignment operator
+	MyMovableClass &operator=(MyMovableClass &&source)  
+    {
+        std::cout << "MOVING (assign) instance " << &source << " to instance " << this << std::endl;
+        if (this == &source)
+            return *this;
+
+        delete[] _data;
+
+        _data = source._data;
+        _size = source._size;
+
+        source._data = nullptr;
+        source._size = 0;
+
+        return *this;
+    }
+	
+	
+	 
 };
 
 
@@ -60,17 +94,44 @@ int main()
     MyMovableClass obj1(10); // regular constructor
     MyMovableClass obj2(obj1); // copy constructor
     obj2 = obj1; // copy assignment operator
-	
-	// Note that when a function returns an object by value, the 
-	// compiler creates a temporary object as an rvalue. 
+	  
 	MyMovableClass obj3 = obj1;
 	// Here, we are instantiating obj3 in the same statement; 
 	// hence the copy assignment operator would not be called.
 
+
+	// The returned value of createObject(10) is passed to the copy 
+	// constructor. The function createObject() returns an instance of
+	// MyMovableClass by value. In such a case, the compiler creates a 
+	// temporary copy of the object as an rvalue, which is passed 
+	// to the copy constructor.
 	MyMovableClass obj4 = createObject(10);
-	// createObject(10) returns a temporary copy of the object as an rvalue, which is passed to the copy constructor.
+   
   
   
+	// The similar two expensive memory operations would be performed with 
+	//        the assignment operator if we execute the following statement
+	
+	// MyMovableClass obj5 = createObject(10); 
+	// obj5 = createObject(10);         // Call to copy assignment operator
   
+    // 		In the above call to copy assignment operator, it would first erase the memory of obj5,
+	// 		then reallocate it during the creation of the temporary object; and then 
+	// 		copy the data from the temporary object to obj5.
+
+	// 		From a performance viewpoint, this code involves far too many copies, making 
+	//		 it inefficient - especially with large data structures.
+	
+	
+	
+	
+
+	// "Move assignment operator" because MyMovableClass(200) is an rvalue reference
+	// obj1 = MyMovableClass(200);  
+	
+	// "Move constructor" because MyMovableClass(300) is an rvalue reference
+    // MyMovableClass obj6 = MyMovableClass(300);  
+	
+	
     return 0;
 }
