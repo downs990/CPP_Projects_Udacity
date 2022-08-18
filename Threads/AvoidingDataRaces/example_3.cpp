3ea15bfe02138b144cabbb360e0dc4f42e9e25eb
Courtney Downs <downscourtney17@gmail.com>
2022-08-18 16:08:37 -0400

    learning about data races with threads

--- Threads/AvoidingDataRaces/example_3.cpp
+++ Threads/AvoidingDataRaces/example_3.cpp
@@ -2,21 +2,6 @@
 #include <thread>
 #include <future>
 
-
-/*
-Description: 
-The basic program structure is mostly identical to the previous example with the object v0 
-being copied by value when passed to the thread function. This time however, even though a
- copy has been made, the original object v0 is modified, when the thread function sets the 
- new name. This happens because the member _name is a pointer to a string and after copying, 
- even though the pointer variable has been duplicated, it still points to the same location 
- as its value (i.e. the memory location) has not changed. 
- 
- Note that when the delay is removed in the thread function, the console output 
- varies between "Vehicle 2" and "Vehicle 3", depending on the system scheduler. Such an error 
- might go unnoticed for a long time. It could show itself well after a program has been 
- shipped to the client - which is what makes this error type so treacherous.
-*/
 class Vehicle
 {
 public:
@@ -31,6 +16,25 @@ public:
     {
         std::cout << "Vehicle #" << _id << " Initializing constructor called" << std::endl;
     }
+
+    // copy constructor 
+    Vehicle(Vehicle const &src)
+    {
+        //...
+        std::cout << "Vehicle #" << _id << " copy constructor called" << std::endl;
+    };
+
+    // move constructor 
+    Vehicle(Vehicle && src)
+    {
+        _id = src.getID();
+        _name = new std::string(src.getName());
+
+        src.setID(0);
+        src.setName("Default Name");
+
+        std::cout << "Vehicle #" << _id << " move constructor called" << std::endl;
+    };
 
     // setter and getter
     void setID(int id) { _id = id; }
@@ -51,11 +55,8 @@ int main()
 
     // launch a thread that modifies the Vehicle name
     std::future<void> ftr = std::async([](Vehicle v) {
-        std::this_thread::sleep_for(std::chrono::milliseconds(500)); // simulate work
         v.setName("Vehicle 2");
-    },v0);
-
-    v0.setName("Vehicle 3");
+    },std::move(v0));
 
     ftr.wait();
     std::cout << v0.getName() << std::endl;
